8b9db3023f8727640a7b19f4c140058d40e4f1db
sb-dor <avaz.shams.2002@gmail.com>
2025-04-12 22:32:33 +0500

    uploaded

--- lib/src/features/location_tracker/helpers/location_tracker_helper.dart
+++ lib/src/features/location_tracker/helpers/location_tracker_helper.dart
@@ -45,10 +45,10 @@ class LocationTrackerHelper {
         return (isValid: false, positionDateTime: null, distance: null);
       }
 
-      // 80 km/h
-      if (currentAndVerifiedPositionsData.speed > 22.22) {
-        return (isValid: false, positionDateTime: null, distance: null);
-      }
+      // // 80 km/h
+      // if (currentAndVerifiedPositionsData.speed > 25.0) {
+      //   return (isValid: false, positionDateTime: null, distance: null);
+      // }
 
       return (
         isValid: true,
